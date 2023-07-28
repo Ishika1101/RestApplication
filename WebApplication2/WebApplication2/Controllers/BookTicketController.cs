4a32230e21f4c718544c369f99c5226b74d3cfc4
ishika <ishikagangwal16@gmail.com>
2023-07-28 16:10:11 +0530

    push app rest api application

---
+++ WebApplication2/WebApplication2/Controllers/BookTicketController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookTicketController : Controller
+    {
+        List<BookTicket> listobj = new List<BookTicket>();
+        [HttpPost]
+        public IActionResult addticket(BookTicket ticket)
+        {
+            BookTicket obj = new BookTicket();
+            obj.Id = ticket.Id;
+            obj.personName = ticket.personName;
+            obj.personEmail = ticket.personEmail;
+            obj.movieDate = ticket.movieDate;
+            obj.movieName = ticket.movieName;
+            obj.ticketCount = ticket.ticketCount;
+
+            listobj.Add(obj);
+            Console.WriteLine(obj);
+            return Ok(obj);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/BookTicket.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models
+{
+    public class BookTicket
+    {
+        public int Id { get; set; }
+        public string personName { get; set; }
+        public string personEmail { get; set; }
+        public string movieName { get; set; }
+        public DateTime movieDate { get; set; }
+
+        public int ticketCount { get; set; }
+    }
+}
